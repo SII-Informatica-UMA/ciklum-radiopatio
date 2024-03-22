6ec2a49e84b0051dd4d5949e8e2587e5b0b8b799
kunSurenio <poncearochasanti@hotmail.com>
2024-03-22 18:48:25 +0100

    ruta ejercicio-editar eliminada

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { EjercicioComponent } from './ejercicio/ejercicio.component';
 import { RutinasComponent } from './rutinas/rutinas.component';
 import { EjercicioDetallesComponent } from './ejercicio-detalles/ejercicio-detalles.component';
-import { EjercicioEditarComponent } from './ejercicio-editar/ejercicio-editar.component';
 
 /* array de objetos con las rutas del resto de componentes */
 export const routes: Routes = [
     {path: 'ejercicio', component: EjercicioComponent},
     {path: 'rutina', component: RutinasComponent},
     {path: 'ejercicio-detalles', component: EjercicioDetallesComponent},
-    {path: 'ejercicio-editar', component: EjercicioEditarComponent},
 ];
