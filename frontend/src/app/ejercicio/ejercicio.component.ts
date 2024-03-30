62f87b6066072a1a713d366da1329e49a41ae11d
piedadpg33 <piedad12g@gmail.com>
2024-03-30 18:43:56 +0100

    creacion del modulo ejercicio

---
+++ frontend/src/app/ejercicio/ejercicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio.component.html',
+  styleUrl: './ejercicio.component.css'
+})
+export class EjercicioComponent {
+
+}
