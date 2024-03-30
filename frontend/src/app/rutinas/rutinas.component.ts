cca49e14a557d49ee08b51b73959a19bbdc2a1a0
kunSurenioRBG <poncearochasanti@hotmail.com>
2024-03-30 18:51:15 +0100

    modulo rutinas added

---
+++ frontend/src/app/rutinas/rutinas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rutinas',
+  standalone: true,
+  imports: [],
+  templateUrl: './rutinas.component.html',
+  styleUrl: './rutinas.component.css'
+})
+export class RutinasComponent {
+
+}
