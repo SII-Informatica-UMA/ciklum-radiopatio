d9a47b178c24676b6cdcf8d92fe26aae74e81a48
kunSurenioRBG <poncearochasanti@hotmail.com>
2024-04-08 23:42:53 +0200

    algunas pruebas unitarias añadidas (4/11 funcionales) (tengo hambre xd ahi lo teneis)

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  describe('AppComponent',  () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [],
+      })
+        .compileComponents();
+    });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +19,35 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ejercicios_rutinas' title`, () => {
+/*   it(`should have the 'ejercicios_rutinas' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ejercicios_rutinas');
   });
-
+ */
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ejercicios_rutinas');
+    expect(compiled.querySelector('h1')?.textContent).toContain('NO PAIN NO GAIN');
+  });
+
+  it('should display carousel with three items', () => {
+    const carouselItems = fixture.debugElement.queryAll(By.css('.carousel-item'));
+    expect(carouselItems.length).toBe(3);
+  });
+
+  it('should trigger verEjercicio method when "EJERCICIOS" button is clicked', () => {
+    spyOn(component, 'verEjercicio');
+    const button = fixture.debugElement.query(By.css('#boton_ejercicio'));
+    button.triggerEventHandler('click', null);
+    expect(component.verEjercicio).toHaveBeenCalled();
+  });
+
+  it('should trigger verRutina method when "RUTINAS" button is clicked', () => {
+    spyOn(component, 'verRutina');
+    const button = fixture.debugElement.query(By.css('#boton_rutina'));
+    button.triggerEventHandler('click', null);
+    expect(component.verRutina).toHaveBeenCalled();
   });
 });
