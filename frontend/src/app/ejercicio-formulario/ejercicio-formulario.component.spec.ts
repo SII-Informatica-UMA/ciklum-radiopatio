5b6e70a6ad4e4ebc5eb90dba1b9726ff900eee41
piedadpg33 <piedad12g@gmail.com>
2024-04-10 19:26:19 +0100

    pruebas formulario

--- frontend/src/app/ejercicio-formulario/ejercicio-formulario.component.spec.ts
+++ frontend/src/app/ejercicio-formulario/ejercicio-formulario.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms'; // Importa FormsModule si lo estás utilizando
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'; // Importa NgbActiveModal si lo estás utilizando
 import { EjercicioFormularioComponent } from './ejercicio-formulario.component';
+import { By } from '@angular/platform-browser';
+// Crea una clase de simulación para NgbActiveModal si es necesario
+class MockNgbActiveModal {
+  // Implementa cualquier método o propiedad que necesites para las pruebas
+}
 
 describe('EjercicioFormularioComponent', () => {
   let component: EjercicioFormularioComponent;
@@ -8,7 +14,12 @@ describe('EjercicioFormularioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EjercicioFormularioComponent]
+      declarations: [ /* No incluyas el componente aquí */ ],
+      imports: [FormsModule], // Asegúrate de importar FormsModule si lo estás utilizando
+      providers: [
+        // Proporciona NgbActiveModal utilizando la clase de simulación si es necesario
+        { provide: NgbActiveModal, useClass: MockNgbActiveModal }
+      ]
     })
     .compileComponents();
     
@@ -20,4 +31,28 @@ describe('EjercicioFormularioComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update the model when input value changes', () => {
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputElement.value = 'Nuevo nombre'; // Simula el cambio de valor en el campo de entrada
+    inputElement.dispatchEvent(new Event('input')); // Dispara el evento de entrada manualmente
+    fixture.detectChanges();
+    
+    expect(component.ejercicio.nombre).toEqual('Nuevo nombre'); // Comprueba que el modelo se haya actualizado correctamente
+  });
+  it('should update the model when textarea value changes', () => {
+    const textareaElement = fixture.debugElement.query(By.css('textarea')).nativeElement;
+    textareaElement.value = 'Nueva descripción'; // Simula el cambio de valor en el textarea
+    textareaElement.dispatchEvent(new Event('input')); // Dispara el evento de entrada manualmente
+    fixture.detectChanges();
+    
+    expect(component.ejercicio.descripcion).toEqual('Nueva descripción'); // Comprueba que el modelo se haya actualizado correctamente
+  });
+
+  
+  
+  
+
+  
 });
+
