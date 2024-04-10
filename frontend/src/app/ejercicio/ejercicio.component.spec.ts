24da7ab6b857ac2e4f0607cf3f1a447d947e4773
piedadpg33 <piedad12g@gmail.com>
2024-04-10 19:27:25 +0100

    pruebas ejercicio

--- frontend/src/app/ejercicio/ejercicio.component.spec.ts
+++ frontend/src/app/ejercicio/ejercicio.component.spec.ts
@@ -1,17 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 import { EjercicioComponent } from './ejercicio.component';
 
+
+import { EjerciciosService } from '../services/ejercicios.service';
+import { BackendService } from '../services/backend.service';
 describe('EjercicioComponent', () => {
   let component: EjercicioComponent;
   let fixture: ComponentFixture<EjercicioComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [EjercicioComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      
+      imports: [HttpClientModule,  EjercicioComponent], // Importa HttpClientModule para proporcionar HttpClient
+      providers: [
+        EjerciciosService, // Proporciona EjerciciosService
+        BackendService // Proporciona BackendService
+      ]
     })
     .compileComponents();
-    
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(EjercicioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +31,14 @@ describe('EjercicioComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call aniadirEjercicio() when button is clicked', () => {
+    spyOn(component, 'aniadirEjercicio');
+    const button = fixture.debugElement.query(By.css('#boton_aniadir'));
+    button.triggerEventHandler('click', null);
+    expect(component.aniadirEjercicio).toHaveBeenCalled();
+  });
+
+  
+  
 });
