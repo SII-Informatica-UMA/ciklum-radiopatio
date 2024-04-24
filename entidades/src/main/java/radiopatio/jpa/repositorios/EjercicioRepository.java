9c9150275de149ead1186eae0fb051c1c7cfedca
piedadpg33 <piedad12g@gmail.com>
2024-04-24 17:22:09 +0100

    consulta correccion

--- entidades/src/main/java/radiopatio/jpa/repositorios/EjercicioRepository.java
+++ entidades/src/main/java/radiopatio/jpa/repositorios/EjercicioRepository.java
@@ -10,20 +10,19 @@ import radiopatio.jpa.entidades.Ejercicio;
 public interface EjercicioRepository extends JpaRepository<Ejercicio,Long> {
     List<Ejercicio> findByNombre(String nombre); //por nombre
     List<Ejercicio> findByTipoAndDificultad(String tipo, String dificultad); //por tipo y dificultad
+
     //por tipo, difiultad y material
     @Query("SELECT e FROM Ejercicio e WHERE e.tipo = :tipo AND e.dificultad = :dificultad AND e.material = :material")
-    List<Ejercicio> findByTipoDificultadYMaterial(@Param("tipo") String tipo, @Param("dificultad") String dificultad, @Param("material") String material);
+    List<Ejercicio> ConsultaTipoDificultadYMaterial(@Param("tipo") String tipo, @Param("dificultad") String dificultad, @Param("material") String material);
     //difiultad mayor o igual a una dada
     @Query("SELECT e FROM Ejercicio e WHERE e.dificultad >= :dificultad")
-    List<Ejercicio> findByDificultadMayorOIgual(@Param("dificultad") String dificultad);
+    List<Ejercicio> ConsultaDificultadMayorOIgual(@Param("dificultad") String dificultad);
     //difiultad menor o igual a una dada
     @Query("SELECT e FROM Ejercicio e WHERE e.dificultad <= :dificultad")
-    List<Ejercicio> findByDificultadMenorOIgual(@Param("dificultad") String dificultad);
+    List<Ejercicio> ConsultaDificultadMenorOIgual(@Param("dificultad") String dificultad);
     
     //para musculos trabajados
     @Query("SELECT e FROM Ejercicio e WHERE e.musculosTrabajados LIKE %:musculo%")
-    List<Ejercicio> findByMusculosTrabajados(@Param("musculo") String musculo);
-
-
+    List<Ejercicio> ConsultaMusculosTrabajados(@Param("musculo") String musculo);
 
 }
